a8a879027d0dd6ff24c4acbd48c000de03c9c906
kimbe7w <beomseok.kim@verizonwireless.com>
2019-08-08 20:54:52 -0400

    entityManager and hibernate

--- spring-jpa-setting/src/main/java/com/github/abstractkim/practice/inflearn/springjpasetting/JpaRunner.java
+++ spring-jpa-setting/src/main/java/com/github/abstractkim/practice/inflearn/springjpasetting/JpaRunner.java
@@ -1,4 +1,33 @@
 package com.github.abstractkim.practice.inflearn.springjpasetting;
 
-public class JpaRunner {
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+        account.setUsername("bskim");
+        account.setPassword("jpa");
+//        entityManager.persist(account);
+        final Session session = entityManager.unwrap(Session.class);
+
+
+        Account accountForSesstion = new Account();
+        accountForSesstion.setUsername("bskim");
+        accountForSesstion.setPassword("hibernate");
+        session.save(accountForSesstion);
+
+    }
 }
