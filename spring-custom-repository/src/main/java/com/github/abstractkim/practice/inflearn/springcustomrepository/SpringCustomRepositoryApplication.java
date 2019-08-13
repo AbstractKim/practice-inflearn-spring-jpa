ff8ac72697ab09b6adddd5279d4f3ce5b0e54a4b
kimbe7w <szone79TALA79!>
2019-08-13 16:01:15 -0400

    init project for spring-custom-repository

---
+++ spring-custom-repository/src/main/java/com/github/abstractkim/practice/inflearn/springcustomrepository/SpringCustomRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.github.abstractkim.practice.inflearn.springcustomrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCustomRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCustomRepositoryApplication.class, args);
+    }
+
+}
---
+++ spring-custom-repository/src/test/java/com/github/abstractkim/practice/inflearn/springcustomrepository/SpringCustomRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.abstractkim.practice.inflearn.springcustomrepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCustomRepositoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
